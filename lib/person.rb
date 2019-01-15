57597d63fe80edf03e9e2e0306fc24515b2145d2
Richard Yance <yance0022@gmail.com>
2019-01-15 21:18:33 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,14 @@
+require 'pry'
 class Person
   #your code here
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
+:weight, :handed, :complexion, :t_shirt_size,
+:wrist_size, :glove_size, :pant_length, :pant_width
+
+  def initialize(attributes)
+    attributes.each do |key, value|
+      self.send("#{key}=", value) #=> self.name("Avi""), ArgumentError given 1 expected 0
+    end
+  end
+
 end
